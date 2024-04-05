ebbb3f01f5f63d2fb8fa5c342b070d0df8ef5656
David Chinea <david.chinea@live.com>
2024-04-04 23:37:51 -0500

    Added a create account function and a auto incrementation reset function

--- main.py
+++ main.py
@@ -11,18 +11,45 @@ def connect_to_database():
         print("Error connecting to the database:", err)
         return None
     
+#Check Balance
+#def check_balance(mydb, account_number):
+    #cursor = mydb.cursor()
+    #cursor.execute("SELECT balance FROM account WHERE account_number = %s", (account_number,))
 
+#Create a new account
+def create_account(mydb,username, password, pin, is_admin=False, user_id = None): #default exceptions
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("INSERT INTO sys.user (user_id,username,password,pin,is_admin) VALUES (%s,%s,%s,%s,%s)", (user_id,username,password,pin,is_admin))
+        mydb.commit()
+        cursor.close()
+        print('Account created successfully')
+    except mysql.connector.Error as err:
+        print('Error creating account:', err)
+        
+#Resets auto incrementation
+def reset_auto_increment(mydb): #
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("ALTER TABLE sys.user AUTO_INCREMENT = 1")
+        mydb.commit()
+        cursor.close()
+        #print('Reset auto incrementation successfully')
+    except mysql.connector.Error as err:
+        print('Error resetting auto incrementation:', err)
+
+# Close connection to MySQL database
 def close_connection(mydb):
     mydb.close()
     print("Connection to the database closed")
 
 
+
 mydb = connect_to_database()
-print(mydb)
+#reset_auto_increment(mydb)
+create_account(mydb,'monkeysBank','bananas',4444, is_admin = True)
+
 close_connection(mydb)
-
-
-
 
 
 
