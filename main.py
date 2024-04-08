ce2e4da8d1f7bac510879d0390daeb5430af1e91
David Chinea <david.chinea@live.com>
2024-04-07 22:22:26 -0500

    Added deposit and withdraw functions, fixed modify function.

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def generate_account_number():
 def check_balance(mydb, account_number, user_id=None):
     try:
         cursor = mydb.cursor()
-        cursor.execute("SELECT balance FROM user WHERE account_number = %s OR user_id = %s", (account_number,user_id))
+        cursor.execute("SELECT balance FROM sys.user WHERE account_number = %s OR user_id = %s", (account_number,user_id))
         balance = cursor.fetchone()[0]
         cursor.close()
         return balance
@@ -29,9 +29,35 @@ def check_balance(mydb, account_number, user_id=None):
         print('Error checking balance:', err)
         return None
 
+# Deposit funds
+def deposit_funds(mydb, account_number, amount):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("UPDATE sys.user SET balance = balance + %s WHERE account_number = %s", (amount, account_number))
+        mydb.commit()
+        cursor.close()
+        print("Funds deposited successfully")
+    except mysql.connector.Error as err:
+       print("Error depositing funds:", err)
+
+def withdraw_funds(mydb, account_number, amount):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("SELECT balance FROM sys.user WHERE account_number = %s", (account_number,))
+        balance = cursor.fetchone()[0]
+        if balance >= amount:
+            cursor.execute("UPDATE sys.user SET balance = balance - %s WHERE account_number = %s", (amount, account_number))
+            mydb.commit()
+            cursor.close()
+            print("Funds withdrawn successfully")
+        else:
+            print("Insufficient funds")
+    except mysql.connector.Error as err:
+        print("Error withdrawing funds:", err)
+
 
 #Create a new account
-def create_account(mydb,username, email, password, pin, is_admin=False,user_id=None,account_number=generate_account_number()): #default exceptions
+def create_account(mydb,username, password, email, pin, is_admin=False,user_id=None,account_number=generate_account_number()): #default exceptions
     try:
         cursor = mydb.cursor()
         cursor.execute("INSERT INTO sys.user (user_id,username,email,password,pin,is_admin,account_number,balance) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", (user_id,username,email,password,pin,is_admin,account_number,0))
@@ -45,7 +71,7 @@ def create_account(mydb,username, email, password, pin, is_admin=False,user_id=N
 def delete_account(mydb,account_number,user_id=None): #default exceptions
     try:
         cursor = mydb.cursor()
-        cursor.execute("DELETE FROM sys.user WHERE account_number = %s OR user_id = %s", (account_number,user_id))
+        cursor.execute("DELETE FROM sys.user WHERE account_number = %s OR user_id = %s", (account_number,user_id)) 
         mydb.commit()
         cursor.close()
         print('Account deleted successfully')
@@ -53,10 +79,19 @@ def delete_account(mydb,account_number,user_id=None): #default exceptions
         print('Error deleting account:', err)
 
 #Modify account details #remeber to add email to modify
-def modify_account(mydb,account_number,username,email, password): 
+def modify_account(mydb,account_number,new_username=None, new_password=None, new_email=None,new_pin=None, is_admin=None): 
     try:
         cursor = mydb.cursor()
-        cursor.execute("UPDATE user SET username = %s, email = %s, password = %s WHERE account_number = %s", (username,email,password,account_number))
+        if new_username is not None:
+            cursor.execute("UPDATE sys.user SET username = %s WHERE account_number = %s", (new_username,account_number))
+        if new_password is not None:
+            cursor.execute("UPDATE sys.user SET password = %s WHERE account_number = %s", (new_password,account_number))
+        if new_email is not None:
+            cursor.execute("UPDATE sys.user SET email = %s WHERE account_number = %s", (new_email,account_number))
+        if new_pin is not None:
+            cursor.execute("UPDATE sys.user SET pin = %s WHERE account_number = %s", (new_pin,account_number))
+        if is_admin is not None:
+            cursor.execute("UPDATE sys.user SET is_admin = %s WHERE account_number = %s", (is_admin,account_number))
         mydb.commit()
         cursor.close()
         print('Modified Account successfully')
@@ -83,10 +118,13 @@ def close_connection(mydb):
 
 mydb = connect_to_database()
 reset_auto_increment(mydb)
-#create_account(mydb,'kevin','kevinballs@gmail.com','bananas',1221)
-modify_account(mydb,110346862,None,None,'socks')
+#create_account(mydb,'kevin','bananas', 'kevinballs@gmail.com',1221)
+#modify_account(mydb,110346862,'kevinRules','socksOff','kevinRules@gmail.com', 1221, False)
 #delete_account(mydb,None,2)
 #print(check_balance(mydb, None, 2))
+#deposit_funds(mydb, 110346862, 20)
+#withdraw_funds(mydb, 110346862, 20)
+#print(check_balance(mydb,110346862))
 
 close_connection(mydb)
 
